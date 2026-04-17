98549c382a115f5d4e1d47206edde192f95f35be
Ryan McCulloch <ryanmcculloch@Ryans-MacBook-Pro.local>
2026-04-17 16:49:08 +0100

    completed 2 level 2 tasks

--- Assessment-Fundamentals-Week-1-main/Level-2/task_two.py
+++ Assessment-Fundamentals-Week-1-main/Level-2/task_two.py
@@ -13,12 +13,39 @@ basket = []
 
 
 def add_to_basket(item: dict) -> list:
-    pass
+    basket.append(item)
+    return basket
+
+
+def multiple_foods_with_count(basket: list) -> list:
+    """This function will remove duplicate items from basket and add count of them"""
+    new_basket = []
+    for item in basket:
+        if item not in new_basket:
+            count = basket.count(item)
+            item["count"] = count
+            new_basket.append(item)
+    if count > 1:
+        for item in new_basket:
+            if item["count"] > 1:
+                item["price"] = item["price"] * item["count"]
+    return new_basket
 
 
 
 def generate_receipt(basket: list) -> str:
-    pass
+    if len(basket) == 0:
+        return "Basket is empty"
+    receipt = ""
+    new_basket = multiple_foods_with_count(basket)
+    for item in new_basket:
+        if item["price"] == 0.0:
+            receipt += f"{item['name']} x {item['count']} - Free\n"
+        else:
+            receipt += f"{item['name']} x {item['count']} - £{item['price']:.2f}\n"
+    total = sum(item['price'] for item in basket)
+    receipt += f"Total: £{total:.2f}"
+    return receipt
 
 #####
 #
